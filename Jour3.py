4c847fe8e653f1aa8e9f69d02c7bf15dc3a2a1e3
Daniel Moreira <134517417+DanielMoreira45@users.noreply.github.com>
2023-12-03 10:32:35 +0100

    aled

--- Jour3.py
+++ Jour3.py
@@ -45,10 +45,46 @@ def day3_part1(fichier):
     matrice = crea_matrice(fichier)
     return traitement(matrice)
 
+def day3_part2(fichier):
+    def crea_matrice(ficher):
+        matrice = []
+        with open(ficher,'r') as f:
+            for ligne in f:
+                matrice.append(list(ligne.strip()))
+        return matrice
+    
+    def nombre(matrice, i ,j):
+        for l in range(i-1,i+2):
+            for k in range(j-1,j+2):
+                print(f" matrice[l][k] {matrice[l][k]}")
+                if l < 0 or k < 0 or l >= len(matrice) or k >= len(matrice[0]):
+                    continue
+                if matrice[l][k].isnumeric():
+                    numero = ""
+                    x = l
+                    y = k
+                    while x < len(matrice) and y < len(matrice[0]) and not (matrice[x][y].isnumeric()) :
+                        
 
+    def traitement(matrice):
+        for i in range(len(matrice)):
+            for j in range(len(matrice[0])):
+                if matrice[i][j] == "*":
+                    print(i,j)
+                    print(nombre(matrice, i ,j))
+
+
+
+
+
+
+
+
+    matrice = crea_matrice(fichier)
+    return traitement(matrice)
 
 print("-------Jour 3--------")
 print("-------Partie 1-------")
 print(day3_part1("input_day3.txt"))
-# print("-------Partie 2-------")
-# print(day3_part2("input_day3.txt"))
+print("-------Partie 2-------")
+print(day3_part2("input.txt"))
